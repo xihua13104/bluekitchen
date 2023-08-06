f0c689f001590acb257cdcba717b513c96c0e60f
xihua13104 <1027901566@qq.com>
2023-08-06 13:15:09 +0800

    3rd-party/nordic-dfu optimize port

--- 3rd-party/nordic-dfu/nordic-dfu.mk
+++ 3rd-party/nordic-dfu/nordic-dfu.mk
@@ -4,8 +4,8 @@ CURDIR := $(patsubst %/, %, $(dir $(MK_PATH)))
 
 NORDIC_DFU_IMG_TOOL := $(CURDIR)/tools/nrfutil-linux
 
-C_SOURCES_NORDIC_DFU := $(CURDIR)/port/nrf_dfu_ble.c
-C_SOURCES_NORDIC_DFU += $(CURDIR)/port/nrf_dfu_flash_port.c
+C_SOURCES_NORDIC_DFU := $(CURDIR)/port/stm32-f4discovery/nrf_dfu_flash_port.c
+C_SOURCES_NORDIC_DFU += $(CURDIR)/src/nrf_dfu_ble.c
 C_SOURCES_NORDIC_DFU += $(CURDIR)/src/nrf_dfu.c
 C_SOURCES_NORDIC_DFU += $(CURDIR)/src/nrf_dfu_transport.c
 C_SOURCES_NORDIC_DFU += $(CURDIR)/src/nrf_dfu_req_handler.c
@@ -20,7 +20,7 @@ C_SOURCES_NORDIC_DFU += $(CURDIR)/src/nrf_dfu_validation.c
 C_SOURCES_NORDIC_DFU += $(CURDIR)/src/nrf_dfu_handling_error.c
 C_SOURCES_NORDIC_DFU += $(CURDIR)/src/crc32.c
 
-C_INCLUDES_NORDIC_DFU := -I$(CURDIR)/port
+C_INCLUDES_NORDIC_DFU := -I$(CURDIR)/port/stm32-f4discovery
 C_INCLUDES_NORDIC_DFU += -I$(CURDIR)/src
 C_INCLUDES_NORDIC_DFU += -I$(CURDIR)/src/nano-pb
 C_INCLUDES_NORDIC_DFU += -I$(CURDIR)/tools
